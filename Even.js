cafad19904839a6495726a07bb7bbcf88d190ae7
Clemmypontsho <clemmypontsho@gmail.com>
2021-06-18 09:40:11 +0200

    determining even and odd numbers

---
+++ Even.js
@@ -0,0 +1,11 @@
+let x = 5;
+let evenOdd = ""
+console.log("Number = " + x);
+
+switch (evenOdd) {
+case  x % 2 == 0 :
+   console.log('Number is even');
+   break;
+ default:
+   console.log('Number is odd');
+}
